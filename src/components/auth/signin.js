5163f9ea1aa2c63d4dbf625ed468be7a71f782c3
Shakespeare89 <growingupcollegestyle@gmail.com>
2020-09-03 12:31:26 -0600

    added signin form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,18 +4,15 @@ import { reduxForm, Field } from 'redux-form';
 import SignInForm from './signinForm';
 
 export default class SignIn extends Component {
-  constructor(props) {
-      super(props)
-
-      this.state = {
-          
-      }
+  onSubmit = (fields) => {
+      console.log(fields);
   }
+  
 
   render() {
       return (
           <div className="sign-in">
-              <SignInForm className='sign-in__form' />
+              <SignInForm onSubmit={this.onSubmit}className='sign-in__form' />
           </div>
       )
   }
